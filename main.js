3c566854fec484bdfedaef19ceddf56eb4b96d9f
Robert <rescobedo12@gmail.com>
2020-08-14 10:53:54 -0700

    Update the background color of grid cells using a 'for' loop.

--- main.js
+++ main.js
@@ -10,9 +10,10 @@ for (let i = 0; i < 16; i++) {
 
 container.style.cssText = "height: 75vh; display: grid; grid-template-columns: repeat(4, 1fr); grid-template-rows: repeat(4, 1fr); grid-gap: 1rem";
 
-const gridCell = document.querySelector('.cell');
-
-
+const gridCells = document.getElementsByClassName('cell');
+for (let c = 0; c < gridCells.length; c++) {
+  gridCells[c].style.background = '#F9E79F';
+};
 
 // Function to clear the current screen and create new grid
 // Should use a <button> and run when clicked
