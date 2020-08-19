033606f48fc7a549f10acaa51d764bf362180c3c
Robert <rescobedo12@gmail.com>
2020-08-19 11:10:44 -0700

    Remove CSS file; create variables for rows, columns, and newGrid; use querySelectorAll instead of getElementsByClassName; removethe click to clear function; add a for loop to remove the old grid.

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
-// Function to create grid
+
 const container = document.querySelector('.container');
+let rows = 16;
+let columns = 16;
+let newGrid;
 
-for (let i = 0; i < 16; i++) {
-  let cell = document.createElement('div');
-  cell.classList.add('cell', `cell-${i}`);
+// Function to create grid
+let makeGrid = function() {
+  for (let i = 0; i < (rows * columns); i++) {
+    let newCell = document.createElement('div');
+    newCell.classList.add('cell', `cell-${i}`);
 
-  container.appendChild(cell);
+    container.appendChild(newCell);
+  };
 };
 
-container.style.cssText = "height: 640px; width: 960px; margin: 0 auto; display: grid; grid-template-columns: repeat(4, 1fr); grid-template-rows: repeat(4, 1fr); grid-gap: ";
+makeGrid();
 
-const gridCells = document.getElementsByClassName('cell');
+// Style the container
+container.style.cssText = `height: 576px; width: 576px; margin: 0 auto; display: grid; grid-template-columns: repeat(${columns}, 1fr); grid-template-rows: repeat(${rows}, 1fr);`
+
+// Style the grid cells
+const gridCells = document.querySelectorAll('.cell');
 for (let c = 0; c < gridCells.length; c++) {
   gridCells[c].style.background = '#F9E79F';
   gridCells[c].style.border = '1px solid black';
@@ -30,16 +40,43 @@ const button = document.createElement('button');
 button.textContent = "Click Me";
 
 buttonContainer.appendChild(button);
-buttonContainer.style.cssText = "margin: 20px auto; text-align: center; border: 1px solid black";
+buttonContainer.style.cssText = "margin: 20px auto; text-align: center";
 button.style.cssText = "margin: 10px; padding: 10px 20px; color: white; background: #5DADE2; border: none";
 
 body.insertBefore(buttonContainer, container);
 
-// Click function
+/*
+
+//THis arrary is what is creating an issue here
+// Click function to clear hover
 button.addEventListener('click', function() {
   for (let i = 0; i < gridCells.length; i++) {
     gridCells[i].style.background = '#F9E79F';
   };
 });
 
-// button will be an event listener that will clear the current grid, send the user a pop-up asking for how many squares per side to make the new grid.
+*/
+
+// Click function to remove old grid and create new grid
+button.addEventListener('click', function() {
+
+  //remove the old divs (newCell)
+  //get a static NodeList OR Use a different loop
+    // the while loop?
+
+  for (let r = 0; r < gridCells.length; r++) {
+    container.removeChild(gridCells[r]);
+  };
+
+  newGrid = Number(prompt("Let's make a new grid. How many squares per side do you want?"));
+  rows = newGrid;
+  columns = newGrid;
+  makeGrid();
+
+  for (let c = 0; c < gridCells.length; c++) {
+    gridCells[c].style.background = '#F9E79F';
+    gridCells[c].style.border = '1px solid black';
+  };
+});
+
+// should I convert the initial grid into a function so that I can call it after I reset it?
